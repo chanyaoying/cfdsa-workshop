8dfabb4614637457cd59b1b12b35d1226754bc12
chanyaoying <me@chanyaoying.com>
2023-10-02 11:00:22 +0800

    push everything

--- day01/db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 3306:3306 \
-  -e MYSQL_ROOT_PASSWORD=abc123 \
-  --mount type=volume,src=myvol,dst=/var/lib/mysql \
-  --name mysql \
-  mysql:8
--- day01/run_netshoot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -ti --rm \
-  --network my-net \
-  nicolaka/netshoot /bin/bash
